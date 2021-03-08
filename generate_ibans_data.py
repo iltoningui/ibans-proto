0f0214cd2f431a8c5ac14cb13a9183dbd78de47f
Ilton ingui <iltoningui@outlook.com>
2021-03-08 23:58:54 +0100

    Refactors generators

--- generate_ibans_data.py
+++ generate_ibans_data.py
@@ -1,116 +1,134 @@
 import re
-import json
+from collections import OrderedDict
 
 from google.protobuf.json_format import ParseDict
 
-from iban_spec_pb2 import IbansSpecs
+import json
+from proto.python.proto.specs_pb2 import IbansSpecs
+
+codes = {
+    "b": "bank_code",
+    "s": "branch_code",
+    "a": "balance_account_number",
+    "i": "account_holder_number",
+    "c": "account_number",
+    "o": "owner_account_number",
+    "m": "currency_code",
+    "t": "account_type"
+}
 
 
 def generate():
-    data = []
-    with open("ibans.txt", "r", encoding="utf8") as file:
+    data = {}
+    ibans = {}
+    with open("data/ibans/ibans.txt", "r", encoding="utf8") as file:
         for line in file:
             if not line or line.startswith("#"):
                 continue
 
             line = line.strip()
             country_code, country_name, length, bban_format, iban_fields = line.split("|")
+            key = country_code
+            filename = country_code[0] + ".dat"
+            if not ibans.get(filename):
+                ibans[filename] = {}
 
-            iban_spec = {"patterns": __get_patterns(bban_format, iban_fields),
-                         "fields": __get_fields(iban_fields),
-                         "country_code": country_code,
-                         "country_name": country_name,
-                         "length": int(length)}
+            iban_spec = __get_patterns(country_code, bban_format, iban_fields)
+            iban_spec["formats"] = __get_format(iban_fields)
+            iban_spec["country_name"] = country_name
+            iban_spec["length"] = length
+            data[key] = iban_spec
+            ibans[filename][key] = iban_spec
 
-            data.append(iban_spec)
-
-    specs_pb = ParseDict({"list": data}, IbansSpecs())
-
-    with open("ibans.json", "w") as file:
+    with open("json/ibans.json", "w") as file:
         json.dump(data, file, indent=4)
 
-    with open("ibans.dat", "wb") as file:
-        file.write(specs_pb.SerializeToString())
+    with open("json/ibans.min.json", "w") as file:
+        json.dump(data, file)
+
+    for filename, bank_map in ibans.items():
+        bank_obj = ParseDict({"specs": bank_map}, IbansSpecs())
+
+        with open(f"bin/ibans/{filename}", "wb+", ) as file:
+            file.write(bank_obj.SerializeToString())
 
 
-def __get_patterns(bban_format: str, iban_fields: str) -> dict:
+def __get_patterns(country_code: str, bban_format: str, iban_fields: str) -> dict:
     iban_fields = iban_fields.replace(" ", "")
-    patterns = {"bban": r"^.{4}", "check_digit": "^.{2}", "constants": []}
+    iban_pattern = ""
+    iban_pattern += f"({country_code})"
+
+    patterns = {"country_code": 1, "check_digit": 2, "bban_pattern": "", "country_check_digits_pattern": ""}
 
     check_digit_field = iban_fields[2:4]
     if check_digit_field == "kk":
-        patterns["check_digit"] += "[0-9]{2}"
-    else:
-        patterns["check_digit"] += check_digit_field
+        check_digit_field = r"[0-9]{2}"
+
+    iban_pattern += f"({check_digit_field})"
+
+    fields = OrderedDict()
+
+    for idx, letter in enumerate(iban_fields[4:], start=4):
+        if letter in codes:
+            if codes[letter] in fields:
+                fields[codes[letter]] += 1
+            else:
+                fields[codes[letter]] = 1
+        else:
+            fields[idx] = letter
+
+    group_number = 3
+    for key, value in fields.items():
+        if isinstance(key, int):
+            iban_pattern += "."
+        else:
+            patterns[key] = group_number
+            iban_pattern += f"(.{{{value}}})"
+            group_number += 1
+
+    patterns["iban_pattern"] = iban_pattern
 
     for x in bban_format.split(","):
         char_type = x[-1]
         size = x[:-1]
         if char_type == 'a':
-            patterns["bban"] += f"[A-Z]{{{size}}}"
+            patterns["bban_pattern"] += f"[A-Z]{{{size}}}"
         elif char_type == 'c':
-            patterns["bban"] += f"[A-Za-z0-9]{{{size}}}"
+            patterns["bban_pattern"] += f"[A-Za-z0-9]{{{size}}}"
         elif char_type == 'n':
-            patterns["bban"] += f"[0-9]{{{size}}}"
-    for match in re.finditer(r"[0-9]+", iban_fields):
+            patterns["bban_pattern"] += f"[0-9]{{{size}}}"
+
+    reserved_fields = re.finditer(r"[0-9]+", iban_fields)
+    if reserved_fields.__sizeof__() > 0:
+        patterns["reserved_fields"] = {"pattern": "", "data": ""}
+        for match in reserved_fields:
+            s = match.start()
+            e = match.end()
+            if s <= 3:
+                continue
+            patterns["reserved_fields"]["pattern"] += f".{{{s}}}([{iban_fields[s:e]}]{{{e - s}}})"
+            patterns["reserved_fields"]["data"] += iban_fields[s:e]
+
+    for match in re.finditer(r"[x]+", iban_fields):
         s = match.start()
         e = match.end()
         if s <= 3:
             continue
-        constant = {"position": s, "constant": iban_fields[s:e]}
-        constant["pattern"] = f"[.]{{{s}}}[{constant['constant']}]{{{e - s}}}"
-        patterns["constants"].append(constant)
+        patterns["country_check_digits_pattern"] += f".{{{s}}}(.{{{e - s}}})"
 
     return patterns
 
 
-def __get_fields(iban_fields: str) -> dict:
-    iban_fields = iban_fields.replace(" ", "")
-    fields = {"bank_code": __get_field_from_pattern(r"[b]+", iban_fields),
-              "account_number": __get_field_from_pattern(r"[c]+", iban_fields),
-              "branch_code": __get_field_from_pattern(r"[s]+", iban_fields),
-              "account_type": __get_field_from_pattern(r"[t]+", iban_fields),
-              "account_holder": __get_field_from_pattern(r"[i]+", iban_fields),
-              "balance_account_number": __get_field_from_pattern(r"[a]+", iban_fields),
-              "owner_account_number": __get_field_from_pattern(r"[o]+", iban_fields),
-              "currency_code": __get_field_from_pattern(r"[m]+", iban_fields),
-              "country_check_digits": __get_field_list_from_pattern(r"[x]+", iban_fields),
-              "constants": __get_field_constants_from_pattern(r"[0-9]+", iban_fields)}
-    return fields
+def __get_format(iban_fields: str) -> dict:
+    blocks = iban_fields.split(" ")
+    iban_format = {"print": {}}
 
-
-def __get_field_from_pattern(pattern, iban_fields):
-    match = re.search(pattern, iban_fields)
-
-    if match is None:
-        return None
-
-    s = match.start()
-    e = match.end()
-
-    field = {"position": s, "length": e - s}
-    return field
-
-
-def __get_field_constants_from_pattern(pattern, iban_fields) -> list:
-    matches = re.finditer(pattern, iban_fields)
-    constants = []
-    for match in matches:
-        s = match.start()
-        e = match.end()
-        if s <= 3:
-            continue
-        constants.append({"position": s, "constant": iban_fields[e:s]})
-    return constants
-
-
-def __get_field_list_from_pattern(pattern, iban_fields) -> list:
-    matches = re.finditer(pattern, iban_fields)
-    country_check_codes = []
-    for match in matches:
-        s = match.start()
-        e = match.end()
-        if s <= 3:
-            continue
-        country_check_codes.append({"position": s, "length": e - s})
-    return country_check_codes
+    format_patterns = []
+    format_groups = []
+    for idx, block in enumerate(blocks, start=1):
+        size = len(block)
+        format_patterns.append(f"(.{{{size}}})")
+        format_groups.append("$" + str(idx))
+    iban_format["print"]["pattern"] = "".join(format_patterns)
+    iban_format["print"]["replacement"] = " ".join(format_groups)
+    return iban_format
